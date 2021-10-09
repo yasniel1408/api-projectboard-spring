1924155342162ece8cb663ca55f9cc501bcbe9e6
Yasniel <yasnielfajardoegues1408@gmail.com>
2021-10-09 16:56:42 -0400

    addPTToBoard method

--- src/main/java/com/infsoft/projectboard/service/ProjectTaskService.java
+++ src/main/java/com/infsoft/projectboard/service/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package com.infsoft.projectboard.service;
 
+import com.infsoft.projectboard.domain.ProjectTask;
 import com.infsoft.projectboard.repository.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,13 @@ public class ProjectTaskService {
 
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask){
+
+        if(projectTask.getStatus() == null || projectTask.getStatus() == "" ){
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
 }
